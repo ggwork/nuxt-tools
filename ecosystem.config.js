c4fd5baad8d28f40fb02f83adb4b0e27f62e1973
郭宪强 <guoxq@wanhuhealth.com>
2022-07-07 14:59:05 +0800

    修改启动命令

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: 'nuxt-tools',
       exec_mode: 'cluster',
       instances: '1', // Or a number of instances
-      script: './node_modules/nuxt/bin/nuxt.js',
+      script: 'cross-env baseUrl=http://stools.moyutime.cn/ --port 3003./node_modules/nuxt/bin/nuxt.js ',
       args: 'start',
       // watch: true, // watch: './',
       // // // 不用监听的文件
